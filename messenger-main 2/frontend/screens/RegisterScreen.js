5db8fca296772434d989b715c714671f025bbfef
ThanushFullStackDeveloper <114086068+ThanushFullStackDeveloper@users.noreply.github.com>
2023-10-15 10:06:27 +0530

    Update RegisterScreen.js

--- messenger-main 2/frontend/screens/RegisterScreen.js
+++ messenger-main 2/frontend/screens/RegisterScreen.js
@@ -27,7 +27,7 @@ const RegisterScreen = () => {
 
     // send a POST  request to the backend API to register the user
     axios
-      .post("http://localhost:8000/register", user)
+      .post("https://deploy-mern-app.vercel.app/register", user)
       .then((response) => {
         console.log(response);
         Alert.alert(
